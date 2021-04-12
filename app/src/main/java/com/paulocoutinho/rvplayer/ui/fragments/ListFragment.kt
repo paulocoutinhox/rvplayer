30b665eca79bd90e5048e9e5b4d6b5af0e5acefa
Paulo Coutinho <paulo@prsolucoes.com>
2021-04-12 14:40:03 -0300

    fix ui state

--- app/src/main/java/com/paulocoutinho/rvplayer/ui/fragments/ListFragment.kt
+++ app/src/main/java/com/paulocoutinho/rvplayer/ui/fragments/ListFragment.kt
@@ -1,8 +1,6 @@
 package com.paulocoutinho.rvplayer.ui.fragments
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -65,10 +63,6 @@ class ListFragment : Fragment(), FragmentLifecycle {
     override fun onResumeFragment() {
         Logger.d("[ListFragment : onResumeFragment]")
         list?.videoPlayerInitializeSurfaceView()
-
-        Handler(Looper.getMainLooper()).post {
-            // list?.videoPlayerPlayFirstAvailable(false)
-        }
     }
 
     override fun onPauseFragment() {
